cc2ec6d8e1a95523401100a46ae08ef30ad65142
dlmarques <daniel.marquesedigital@gmail.com>
2023-09-03 22:21:08 +0100

    integrate authenticationauth0

--- src/modules/login/Login.tsx
+++ src/modules/login/Login.tsx
@@ -1,9 +1,19 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { Spinner } from '@chakra-ui/react';
+import styles from './login.module.scss';
+import { ReactComponent as Logo } from '../../assets/logo/Main.svg';
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
   loginWithRedirect();
-  return <div>Loading...</div>;
+  return (
+    <div className={styles.loading}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <Spinner size="xl" />
+    </div>
+  );
 };
 
 export default Login;
