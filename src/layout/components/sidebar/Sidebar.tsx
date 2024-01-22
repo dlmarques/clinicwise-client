66963e885e0484f3478e12bec2edc66e55404c38
dlmarques <daniel.marquesedigital@gmail.com>
2024-01-22 00:28:46 +0000

    sidebar for each role

--- src/layout/components/sidebar/Sidebar.tsx
+++ src/layout/components/sidebar/Sidebar.tsx
@@ -1,46 +1,46 @@
 import styles from './sidebar.module.scss';
-import { useNavigate } from 'react-router-dom';
 import { Button } from '@chakra-ui/react';
 import { ReactComponent as LightLogo } from '../../../assets/logo/Main.svg';
 import { ReactComponent as DarkLogo } from '../../../assets/logo/Inverted.svg';
-import { FaMoon, FaPlus, FaSun } from 'react-icons/fa';
+import { FaMoon, FaSun } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { getUserTheme } from '../../../store/selectors';
+import { getUserRole, getUserTheme } from '../../../store/selectors';
 import { userActions } from '../../../store/user/user';
+import { ROLES } from '../../../utils/roles';
+import AdminSidebarContent from '../../../modules/admin/sidebar-content/AdminSidebarContent';
+import DoctorSidebarContent from '../../../modules/doctor/sidebar-content/DoctorSidebarContent';
+import PatientSidebarContent from '../../../modules/patient/sidebar-content/PatientSidebarContent';
+import OwnerSidebarContent from '../../../modules/owner/sidebar-content/OwnerSidebarContent';
 
 const Sidebar = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const theme = useSelector(getUserTheme);
+  const role = useSelector(getUserRole);
+
+  const renderSidebarContentByRole = () => {
+    switch (role) {
+      case ROLES.ADMIN:
+        return <AdminSidebarContent key={role} styles={styles} />;
+      case ROLES.DOCTOR:
+        return <DoctorSidebarContent key={role} />;
+      case ROLES.PATIENT:
+        return <PatientSidebarContent key={role} styles={styles} />;
+      case ROLES.OWNER:
+        return <OwnerSidebarContent key={role} />;
+
+      default:
+        break;
+    }
+  };
 
   return (
     <div className={styles.sidebar}>
       <div className={styles.logo}>
         {theme === 'dark' ? <DarkLogo /> : <LightLogo />}
       </div>
-      <div className={styles.menu}>
-        <Button leftIcon={<FaPlus />} className={styles.button}>
-          {t('schedule')}
-        </Button>
-        <ul>
-          <li>
-            <span onClick={() => navigate('settings')}>{t('search')}</span>
-          </li>
-          <li>
-            <span onClick={() => navigate('settings')}>
-              {t('appointment_book')}
-            </span>
-          </li>
-          <li>
-            <span onClick={() => navigate('settings')}>{t('documents')}</span>
-          </li>
-          <li>
-            <span onClick={() => navigate('settings')}>{t('my_profile')}</span>
-          </li>
-        </ul>
-      </div>
+      <div className={styles.menu}>{renderSidebarContentByRole()}</div>
       <div className={styles.theme}>
         <Button
           className={styles.button}
